a7607ffc90937c6f7f25c0e171a823525b68274f
sudheeradh <k.sudheeradh@gmail.com>
2023-03-31 06:10:49 +0530

    dog pics css: added flex

--- Dog Photos/js/main.js
+++ Dog Photos/js/main.js
@@ -1,13 +1,14 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
-dogImage = fetch('https://dog.ceo/api/breeds/image/random')
-              .then(res => res.json())
-              .then(data => insertImage(data.message))
-              .catch(err => console.error(err))  
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then(data => insertImage(data.message))
+    .catch(err => console.error(err))  
 
 
 function insertImage(imgLink) {
     imgElement = document.querySelector('img');
     imgElement.setAttribute('src', imgLink);
+    imgElement.onload = () => {imgElement.classList.remove('hidden');}
 }
 
 function refresh() {
