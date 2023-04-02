1e565574529ed88bb339ed704a23da36c2f87b60
sudheeradh <k.sudheeradh@gmail.com>
2023-04-02 20:19:13 +0530

    Memory: Init

--- Memory/js/main.js
+++ Memory/js/main.js
@@ -1,5 +1,6 @@
 const nums = []
 let size = 4;
+const board = document.querySelector('#board');
 
 for (let j = 0; j < 16; j++) {
     nums.push(j);
@@ -14,6 +15,19 @@ let random = shuffle(nums).slice(0, Math.floor(size * size / 2));
 
 for (let i = 0; i < random.length; i++) {
     random[i] += '.jpg';
+}
+
+let k = 0;
+for (let i = 0; i < size; i++) {
+    row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < size; j++) {
+        cell = document.createElement('div');
+        cell.classList.add('cell', k);
+        row.appendChild(cell);
+        k++;
+    }
+    board.appendChild(row);
 }
 
 console.log(random)
